ee80b75911e76ab29a2808e49339a20002d76955
Shokirjon Salaev <salayevshokirjon785@gmail.com>
2025-12-06 16:27:45 +0500

    Add files via upload

---
+++ girma.py
@@ -0,0 +1,153 @@
+# # 1- masala
+#
+# def kvadrat(son):
+#     x = son ** 2
+#     return x
+# x = int(input("Sonni kiriting:"))
+# print(f"{x} sonining kvadrati: {kvadrat(x)}")
+from idlelib.colorizer import matched_named_groups
+
+
+
+
+# 2- masala
+
+# def takrorlash(matn, n):
+#     return matn * n
+# print(takrorlash("salom", 3))
+
+
+
+
+
+ # 3-masala
+# def juftmi(x):
+#        if x % 2 == 0:
+#            return f"javob: juft"
+#        elif x % 2 == 1:
+#            return f"javob: toq"
+# x = int(input("X ni kiriting:"))
+# print(juftmi(x))
+
+
+
+# 4 masala
+# def hisoblash(x,y,belgi):
+#     if belgi  == "+":
+#         return x + y
+# x=int(input("X ni kiriting:"))
+# y=int(input("Y ni kiriting:"))
+# belgi=input("BELGI ni kiriting:")
+# print(hisoblash(x,y,belgi))
+
+
+
+
+
+ #5 - masala
+# def uzunlik(matn):
+#     hisob = 0
+#     for harf in matn:
+#         hisob += 1
+#     return f"Uzunlik:{hisob}"
+# matn =input("Matnni kiriting:")
+#
+# print(uzunlik(matn))
+
+
+
+# 6 -masala
+# def son_turi(son):
+#
+#     if son > 0:
+#         return "musbat"
+#     elif son < 0:
+#         return "manfiy"
+#     else:
+#         return "nol"
+# print(son_turi(42))
+# print(son_turi(-7))
+
+
+
+
+
+# 7 -masala
+# def abs_qiymat(son):
+#
+#     if son < 0:
+#         return -son
+#     else:
+#         return son
+# print(abs_qiymat(-7))
+# print(abs_qiymat(12))
+# print(abs_qiymat(0))
+
+
+
+
+# 8- masala
+# def teskari_matn(matn):
+#     return matn[::-1]
+# print(teskari_matn("salom"))
+
+# 2  modul
+# 1 -masala
+
+# def faktorial(n):
+#
+#     if n == 0 or son == 1:
+#         return 1
+#     else:
+#         return n * faktorial(n -1)
+# print(faktorial(5))
+
+
+
+
+# 2 -masala
+# def juft_sum(royxat):
+#     yigindi = 0
+#     for son in royxat:
+#         if son % 2 == 0:
+#             yigindi += son
+#     return yigindi
+# royxat = [1,2,3,4,6]
+# print(juft_sum(royxat))
+# 3- masala
+# def daraja(son):
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+#
+# # 4-masala
+#  def daraja (son):
+#     n = son **("son1")
+#     return n
+#  son  = int(input("sonni kiriting"))
+# son = int(input("N"))
+#
+#
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
